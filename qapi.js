ae6519e0449907adf015c8a885e46c6c2aa03556
rikhwan fatih <87036188+rikhwanfatih@users.noreply.github.com>
2021-08-02 02:11:00 +0700

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60f6bfd7b42a9a0018931beb",
+				"60f6bfd7b42a9a0018931bed"
+			]
+		},
+		"60f6bfd7b42a9a0018931beb": {
+			"id": "60f6bfd7b42a9a0018931beb",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60f6bfd7b42a9a0018931bed": {
+			"id": "60f6bfd7b42a9a0018931bed",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
